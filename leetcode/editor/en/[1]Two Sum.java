a196f7180390060363083e6a370ccbc704864585
Kiki <shiqi.su@student.uq.edu.au>
2025-09-24 11:35:40 +1000

    Add Leetcode to the project

---
+++ leetcode/editor/en/[1]Two Sum.java
@@ -0,0 +1,97 @@
+//Given an array of integers nums and an integer target, return indices of the 
+//two numbers such that they add up to target. 
+//
+// You may assume that each input would have exactly one solution, and you may 
+//not use the same element twice. 
+//
+// You can return the answer in any order. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [3,2,4], target = 6
+//Output: [1,2]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: nums = [3,3], target = 6
+//Output: [0,1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 2 <= nums.length <= 10⁴ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// -10⁹ <= target <= 10⁹ 
+// Only one valid answer exists. 
+// 
+//
+// 
+//Follow-up: Can you come up with an algorithm that is less than 
+//O(n²)
+// time complexity?
+//
+// Related Topics Array Hash Table 👍 62933 👎 2276
+
+import java.util.HashMap;
+
+/**
+ * @author Shiqi Su
+ * @description: Two Sum -- different solutions.
+ * @date 2025-07-22 10:44:33
+ */
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    /**
+     * Approach 1 -- Brute Force
+     * @param nums
+     * @param target
+     * @return
+     */
+//    public int[] twoSum(int[] nums, int target) {
+//        int n = nums.length;
+//        for (int i = 0; i < n; i++) {
+//            for (int j = i + 1; j < n; j++) {
+//                if (nums[j] + nums[i] == target) {
+//                    return new int[] { i, j };
+//                }
+//            }
+//        }
+//        return new int[] {};
+//    }
+
+    /**
+     * Approach 2 -- Two-pass Hash Table
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] {i, map.get(complement)};
+            }
+        }
+        return new int[] {};
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
