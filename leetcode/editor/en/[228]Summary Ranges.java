9be2d6d488586c46cdd0fed37be899b6568eebd3
Kiki <shiqi.su@student.uq.edu.au>
2026-02-12 20:20:44 +1000

    Feat(Algo): LeetCode 228, 349

---
+++ leetcode/editor/en/[228]Summary Ranges.java
@@ -0,0 +1,78 @@
+//You are given a sorted unique integer array nums. 
+//
+// A range [a,b] is the set of all integers from a to b (inclusive). 
+//
+// Return the smallest sorted list of ranges that cover all the numbers in the 
+//array exactly. That is, each element of nums is covered by exactly one of the 
+//ranges, and there is no integer x such that x is in one of the ranges but not in 
+//nums. 
+//
+// Each range [a,b] in the list should be output as: 
+//
+// 
+// "a->b" if a != b 
+// "a" if a == b 
+// 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [0,1,2,4,5,7]
+//Output: ["0->2","4->5","7"]
+//Explanation: The ranges are:
+//[0,2] --> "0->2"
+//[4,5] --> "4->5"
+//[7,7] --> "7"
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [0,2,3,4,6,8,9]
+//Output: ["0","2->4","6","8->9"]
+//Explanation: The ranges are:
+//[0,0] --> "0"
+//[2,4] --> "2->4"
+//[6,6] --> "6"
+//[8,9] --> "8->9"
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 0 <= nums.length <= 20 
+// -2³¹ <= nums[i] <= 2³¹ - 1 
+// All the values of nums are unique. 
+// nums is sorted in ascending order. 
+// 
+//
+// Related Topics Array 👍 4495 👎 2377
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> list = new ArrayList<>();
+        if (nums.length == 0) {
+            return list;
+        }
+
+        int j = nums[0];
+        for (int i = 1; i <= nums.length; i++) {
+            if (i == nums.length || nums[i] != nums[i - 1] + 1) {
+                if (j == nums[i - 1]) {
+                    list.add(String.valueOf(j));
+                } else {
+                    list.add(j + "->" + nums[i - 1]);
+                }
+                if (i < nums.length) {
+                    j = nums[i];
+                }
+            }
+        }
+        return list;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode/editor/en/[349]Intersection of Two Arrays.java
@@ -0,0 +1,49 @@
+//Given two integer arrays nums1 and nums2, return an array of their 
+//intersection. Each element in the result must be unique and you may return the result in 
+//any order. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums1 = [1,2,2,1], nums2 = [2,2]
+//Output: [2]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+//Output: [9,4]
+//Explanation: [4,9] is also accepted.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums1.length, nums2.length <= 1000 
+// 0 <= nums1[i], nums2[i] <= 1000 
+// 
+//
+// Related Topics Array Hash Table Two Pointers Binary Search Sorting 👍 6830 👎
+// 2343
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> result = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            set.add(nums1[i]);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                result.add(nums2[i]);
+            }
+        }
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
